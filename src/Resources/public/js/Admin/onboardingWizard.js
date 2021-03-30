9f525fbed0e46ba1f73715fbcc5446cba0aeab64
Marcin Gromek <marcin.gromek@bitbag.pl>
2021-03-30 19:46:30 +0200

    feature:MOL-101: Add 1 step, modify modal position, scrollToHandler tweaks

--- src/Resources/public/js/Admin/onboardingWizard.js
+++ src/Resources/public/js/Admin/onboardingWizard.js
@@ -1,24 +1,46 @@
 $(function () {
+  const container = document.querySelector('[name=sylius_payment_method]');
+
+  if (!container) {
+    return;
+  }
+
   const steps = [
+    // {
+    //   stepNoClass: 'step-1',
+    //   classActive: 'active',
+    //   text: 'Onboarding Assistant Designs',
+    //   btnBackText:'Skip this',
+    //   btnBackClass:'d-none',
+    //   btnNextText: 'Start guide',
+    //   btnNextClass:'ml-auto mr-auto',
+    //   attachToElement: 'body'
+    // },
     {
+      stepNoClass: 'step-2',
       title: '<h2>Let me help you</h2>',
       text: 'Thank you for installing Mollie for payment services. This guide will take you through the configuration setup. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam suscipit nibh quis urna congue, et interdum nulla rutrum. Cras at justo ornare.',
       btnBackText:'Skip this',
       btnNextText: 'Start guide',
+      scrollToTarget: '#sylius_payment_method_gatewayConfig_config_api_key_test'
     },
     {
+      stepNoClass: 'step-3 modal-onboarding',
       attachToElement: '.ui.dropdown.selection',
+      scrollToTarget: '.ui.dropdown.selection',
       text: 'TEST will be the default in the plugin. You only need to do the configuration once to have TEST + LIVE environments available. Try easily togging between the two.',
       btnBackText:'Go back',
       btnNextText: 'Next',
     },
     {
+      stepNoClass: 'step-4',
       title: '<h2>Connect to your account</h2>',
       text: 'To sync the Mollie plugin to your webshop you\'ll hneed Mollie API keys and Profile ID.',
       btnBackText:'Login to my account',
       btnNextText: 'Create a Mollie account',
     },
     {
+      stepNoClass: 'step-5 modal-onboarding',
       attachToElement: '[for="sylius_payment_method_gatewayConfig_config_api_key_test"] + *',
       text: 'Fill in your correct details and click "TEST API Key" this will return a successful or failed result for both the LIVE and TEST environments.\n' +
         '\n' +
@@ -27,14 +49,16 @@ $(function () {
       btnNextText: 'Next',
     },
     {
+      stepNoClass: 'step-6 modal-onboarding',
       text: 'Enabling components, allows you to add the fields needed for credit card holder data to your own\n' +
         'checkout. If you select NO, users will be redirected to the Mollie\n' +
         'checkout page',
       btnBackText:'Go back',
       btnNextText: 'Next',
-      attachToElement: '[for="sylius_payment_method_gatewayConfig_config_components"]'
+      attachToElement: '.mollie-components',
     },
     {
+      stepNoClass: 'step-7',
       title: '<h2>Let me help you</h2>',
       text: '',
       btnBackText:'Go back',
@@ -42,6 +66,10 @@ $(function () {
       attachToElement: {}
     },
   ];
+
+  function close() {
+
+  }
 
   const stepss = [
     {
@@ -62,7 +90,6 @@ $(function () {
           text: 'Start onboarding assistant'
         }
       ],
-      id: 'wrapper',
       when: {
         show() {
           // const currentStepElement = this.el;
@@ -202,37 +229,45 @@ $(function () {
   const tour = new Shepherd.Tour({
     useModalOverlay: true,
     defaultStepOptions: {
+      arrow: false,
       cancelIcon: {
-        enabled: true
+        enabled: true,
       },
-      classes: 'modal-onboarding',
       scrollTo: {
         behavior: 'smooth',
       }
-    }
+    },
+    id: 'wrapper',
   });
 
   steps.forEach((step, index) => {
-    console.log(step.attachToElement);
     tour.addStep({
       title: step.title ? step.title : null,
       text: step.text,
+      classes: step.stepNoClass,
       attachTo: {
         ...(step.attachToElement && { element: step.attachToElement }),
-        ...(step.attachToElement && { on: 'bottom' }),
+      },
+      ...(step.classActive && { highlightClass: step.classActive }),
+      scrollToHandler: function () {
+        const target = document.querySelector(step.scrollToTarget);
+        console.log(target.getBoundingClientRect().top);
+        window.scrollTo(0, target.getBoundingClientRect().top - 200);
       },
       buttons: [
         {
           text: step.btnBackText,
-          action: tour.back,
-          secondary: true
+          action: index === 0 ? tour.cancel : tour.back,
+          secondary: true,
+          ...(step.btnBackClass && { classes: step.btnBackClass }),
         },
         {
           text: step.btnNextText,
           action: index === steps.length - 1 ? tour.complete : tour.next,
-          // classes: 'btn btn__dc--light',
+          ...(step.btnNextClass && { classes: step.btnNextClass }),
         },
       ],
+      ...(step.when && { when: step.when })
     });
   });
 
