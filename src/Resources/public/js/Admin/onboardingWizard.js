2c702a411484f97cebdbe481a299520b3beaed7e
Marcin Gromek <marcin.gromek@bitbag.pl>
2021-04-09 17:11:54 +0200

    feature:MOL-101: Fix orderApi steps (without modal overlay still) - in progress

--- src/Resources/public/js/Admin/onboardingWizard.js
+++ src/Resources/public/js/Admin/onboardingWizard.js
@@ -3,15 +3,12 @@ $(function () {
     return;
   }
 
-  const navbar = document.querySelector('.onboardingWizard-nav');
-  const navbarItems = [...navbar.querySelectorAll('.onboardingWizard-nav-item')];
-
   const steps = [
     {
       id: 'step-start',
       text: 'Thank you for installing Mollie for payment services. This guide will take you through the configuration setup. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam suscipit nibh quis urna congue, et interdum nulla rutrum. Cras at justo ornare.',
-      title: '<h2>Let me help you</h2>',
-      stepNoClass: 'step-2',
+      title: 'Let me help you',
+      stepClass: 'step-2',
       btnBackText:'Skip this, I know how it works',
       btnNextText: 'Start onboarding assistant <i class="icon angle right"></i>',
       scrollToTarget: '#sylius_payment_method_gatewayConfig_config_api_key_test',
@@ -19,10 +16,9 @@ $(function () {
     },
     {
       text: 'TEST will be the default in the plugin. You only need to do the configuration once to have TEST + LIVE environments available. Try easily togging between the two.',
-      stepNoClass: 'right-bottom',
+      stepClass: 'right-bottom',
       classActive: 'api-settings',
       attachToElement: '.onboardingWizard-environment',
-      scrollToTarget: '.ui.dropdown.selection',
       btnBackText:'Go back',
       btnNextText: 'Next <i class="icon angle right"></i>',
       btnNextClass: 'with-triangle',
@@ -31,7 +27,7 @@ $(function () {
     {
       title: 'Connect to your Mollie account',
       text: 'To sync the Mollie plugin to your webshop you\'ll need Mollie API keys and Profile ID.',
-      stepNoClass: 'step-4',
+      stepClass: 'step-4',
       classActive: 'api-settings',
       btnBackText:'Login to my account',
       btnNextText: 'Create a Mollie account <i class="icon angle right"></i>',
@@ -42,7 +38,7 @@ $(function () {
       text: 'Fill in your correct details and click "TEST API Key" this will return a successful or failed result for both the LIVE and TEST environments.\n' +
         '\n' +
         'Learn about the difference between: Orders API or the Payments API',
-      stepNoClass: 'step-5 right-bottom',
+      stepClass: 'step-5 right-bottom',
       classActive: 'api-settings',
       btnBackText:'Go back',
       btnNextText: 'Next <i class="icon angle right"></i>',
@@ -52,7 +48,7 @@ $(function () {
     },
     {
       text: 'Webshop checkout Configurations, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce semper velit et urna gravida efficitur.',
-      stepNoClass: 'step-6 right-bottom',
+      stepClass: 'step-6 right-bottom',
       classActive: 'store-settings',
       btnBackText: 'Go back',
       btnNextText: 'Next',
@@ -62,17 +58,17 @@ $(function () {
       text: 'Enabling components, allows you to add the fields needed for credit card holder data to your own\n' +
         'checkout. If you select NO, users will be redirected to the Mollie\n' +
         'checkout page',
-      stepNoClass: 'step-6 right-bottom',
+      stepClass: 'right-bottom',
       classActive: 'store-settings',
       btnBackText:'Go back',
       btnNextText: 'Next <i class="icon angle right"></i>',
       btnNextClass: 'with-triangle',
-      attachToElement: '#sylius_payment_method_gatewayConfig_config_components + label',
+      attachToElement: '.onboardingWizard-components',
       btnCollapseClass: 'btn-collapse',
     },
     {
       text: 'Enabling single click payments remembers your consumer\'s payment preferences in order to expedite follow-up payments. Your consumer does not have to perform any additional actions to enjoy quick and easy payments.',
-      stepNoClass: 'step-7 right-bottom',
+      stepClass: 'step-7 right-bottom',
       classActive: 'store-settings',
       btnBackText:'Go back',
       btnNextText: 'Next <i class="icon angle right"></i>',
@@ -82,7 +78,7 @@ $(function () {
     },
     {
       text: 'We\'ll go through setup with the Payments API first and then highlight differences if you choose to use the Orders API',
-      stepNoClass: 'step-8 right-bottom',
+      stepClass: 'step-8 right-bottom',
       classActive: 'payment-settings',
       btnBackText:'Go back',
       btnNextText: 'Next',
@@ -90,7 +86,7 @@ $(function () {
     },
     {
       text: 'You can enter a custom title here - it will be displayed on your checkout page',
-      stepNoClass: 'step-9 right-bottom',
+      stepClass: 'step-9 right-bottom',
       classActive: 'payment-settings',
       btnBackText:'Go back',
       btnNextText: 'Next <i class="icon angle right"></i>',
@@ -100,7 +96,7 @@ $(function () {
     },
     {
       text: 'Choose Payments API (Payments API can not be used for methods such as Klarna - we\'ll set-up that up later) Learn about the difference  between Orders APIor the Payments API',
-      stepNoClass: 'step-10 right-bottom',
+      stepClass: 'step-10 right-bottom',
       classActive: 'payment-settings',
       btnBackText:'Go back',
       btnNextText: 'Next <i class="icon angle right"></i>',
@@ -110,15 +106,16 @@ $(function () {
     },
     {
       text: 'When using Payments API you may want additional details to help you match payments with customer orders -- you can enter those values here but make sure to use the correct tags provide in the text below',
-      stepNoClass: 'step-11 right-bottom',
+      stepClass: 'step-11 right-bottom',
       classActive: 'payment-settings',
       btnBackText:'Go back',
       btnNextText: 'Next',
       btnCollapseClass: 'btn-collapse',
     },
     {
+      id: 'restrictPayment',
       text: 'Restrict/ allow payment per individual countries.',
-      stepNoClass: 'step-12 right-bottom',
+      stepClass: 'step-12 right-bottom',
       classActive: 'payment-settings',
       btnBackText:'Go back',
       btnNextText: 'Next <i class="icon angle right"></i>',
@@ -129,7 +126,7 @@ $(function () {
     },
     {
       text: 'In case you have fees that you are passing on to the consumer, you can add them here',
-      stepNoClass: 'step-13 right-bottom',
+      stepClass: 'step-13 right-bottom',
       classActive: 'payment-settings',
       btnBackText:'Go back',
       btnNextText: 'Next <i class="icon angle right"></i>',
@@ -140,7 +137,7 @@ $(function () {
     },
     {
       text: 'Upload a custom image for the payment method icon, this will be shown in the checkout page',
-      stepNoClass: 'step-14 right-bottom',
+      stepClass: 'step-14 right-bottom',
       classActive: 'payment-settings',
       btnBackText:'Go back',
       btnNextText: 'Next <i class="icon angle right"></i>',
@@ -150,7 +147,7 @@ $(function () {
     },
     {
       title: '<i class="icon check circle"></i> You\'re all set!',
-      stepNoClass: 'step-15',
+      stepClass: 'step-15',
       classActive: 'payment-settings',
       text: 'You\'re all done, you can now attempt a consumer order or your website',
       btnBackClass: 'd-none',
@@ -158,27 +155,80 @@ $(function () {
       btnNextText: 'Start using Mollie plugin <i class="icon angle right"></i>',
       btnCollapseClass: 'btn-collapse d-none',
     },
-    {
+
+  ];
+
+  const stepQuitConfirmation = (previousStepIndex) => {
+    return {
       id: 'step-quitConfirmation',
       title: 'Are you sure you want to quit ?',
       text: 'You\'re all done, you can now attempt a consumer order or your website',
-      btnNextClass: 'mr-auto js-onboarding-continue',
-      btnBackClass: 'js-onboarding-quit',
-      btnNextText: 'Continue onboarding <i class="icon angle right"></i>',
-      btnBackText: 'Quit the onboarding assistant',
-      btnCollapseClass: 'btn-collapse d-none',
-    },
-  ];
+      buttons: [
+        {
+          text: 'Quit the onboarding assistant',
+          action: () => {
+            tour.complete();
+          },
+          secondary: true,
+          classes: 'js-onboarding-quit',
+        },
+        {
+          text: 'Continue onboarding <i class="icon angle right"></i>',
+          action: () => {
+            tour.show(previousStepIndex, true);
+          },
+          classes: 'js-onboarding-continue',
+        },
+      ],
+    }
+  }
 
-  // function skipStep(step, forward) {
-  //   const index = this.steps.indexOf(step);
-  //   const nextIndex = forward ? index + 1 : index - 1;
-  //   if (nextIndex === this.steps.length - 1) {
-  //     this.complete();
-  //   } else {
-  //     this.show(nextIndex, forward);
-  //   }
-  // }
+  const navbar = document.querySelector('.onboardingWizard-nav');
+
+  const navbarVisibilityHandler = () => {
+    navbar.classList.toggle('d-none', !tour.isActive());
+    navbar.setAttribute('aria-hidden', tour.isActive());
+  }
+
+  const navbarProgressHandler = (currentStep) => {
+    const navbarItems = [...navbar.querySelectorAll('.onboardingWizard-nav-item')];
+
+    navbarItems.some((navItem) => {
+      const { navigationStep } = navItem.dataset;
+
+      if ([...currentStep.classList].includes(navigationStep)) {
+        navbarItems.forEach(item => {
+          item.classList.remove('active');
+        })
+        navItem.classList.add('active');
+
+        return true
+      }
+
+      return false;
+    });
+  }
+
+  const modalCollapseHandler = (tour) => {
+    const currentStep = tour.currentStep.el;
+    const buttonCollapse = currentStep.querySelector('.btn-collapse');
+    const isCollapsed = [...currentStep.classList].includes('collapsed');
+    const paragraph = document.createElement('span');
+
+    paragraph.classList.add('btn-text-open');
+    paragraph.textContent = 'Open';
+
+    if (!buttonCollapse) {
+      return;
+    }
+
+    const textOpen = buttonCollapse.querySelector('.btn-text-open ')
+
+    !isCollapsed ? buttonCollapse.appendChild(paragraph) : buttonCollapse.removeChild(textOpen)
+
+    currentStep.classList.toggle('collapsed', !isCollapsed);
+    currentStep.setAttribute('aria-hidden', !isCollapsed);
+  }
 
   const tour = new Shepherd.Tour({
     useModalOverlay: true,
@@ -190,18 +240,18 @@ $(function () {
         enabled: true,
       },
       scrollTo: { behavior: 'smooth', block: 'center' },
+      modalOverlayOpeningRadius: 4,
     },
   });
 
-  tour.on('show', () => navbar.classList.remove('d-none'));
-  ['complete', 'cancel'].forEach(event => tour.on(event, () => navbar.classList.add('d-none')))
+  ['inactive', 'show'].forEach(event => tour.on(event, navbarVisibilityHandler));
 
   steps.forEach((step, index) => {
     tour.addStep({
       id: step.id,
       title: step.title ? step.title : null,
       text: step.text,
-      classes: step.stepNoClass,
+      classes: step.stepClass,
       attachTo: {
         ...(step.attachToElement && { element: step.attachToElement }),
         on: 'top-start'
@@ -212,74 +262,33 @@ $(function () {
           const currentStep = this.tour.getCurrentStep().target;
 
           if (!currentStep) {
-            return false;
+            return;
           }
 
-          navbarItems.some((navItem) => {
-            const { navigationStep } = navItem.dataset;
-
-            if ([...currentStep.classList].includes(navigationStep)) {
-              navbarItems.forEach(item => {
-                item.classList.remove('active');
-              })
-              navItem.classList.add('active');
-
-              return true
-            }
-
-            return false;
-          });
+          navbarProgressHandler(currentStep);
         },
         cancel() {
           const previousStepIndex = tour.steps.indexOf(tour.getCurrentStep());
 
-          tour.show('step-quitConfirmation', true);
+          tour.addStep(stepQuitConfirmation(previousStepIndex));
 
+          tour.show('step-quitConfirmation', true);
           const currentElement = tour.currentStep.el;
           const buttonClose = currentElement.querySelector('.shepherd-cancel-icon');
           buttonClose.classList.add('d-none');
-
-          document.querySelector('.js-onboarding-continue').addEventListener('click', () => {
-            tour.show(previousStepIndex, true);
-            return;
-          })
-
-          document.querySelector('.js-onboarding-quit').addEventListener('click', () => {
-            tour.complete();
-            return;
-          })
         }
       },
       buttons: [
         {
           text: '<i class="arrow down icon"></i>',
-          action() {
-            const currentStep = this.currentStep.el;
-            const buttonCollapse = currentStep.querySelector('.btn-collapse');
-            const isCollapsed = [...currentStep.classList].includes('collapsed');
-
-            const paragraph = document.createElement('span');
-            paragraph.classList.add('btn-text-open');
-            paragraph.textContent = 'Open';
-
-            if (!buttonCollapse) {
-              return;
-            }
-
-            const textOpen = buttonCollapse.querySelector('.btn-text-open ')
-
-            !isCollapsed ? buttonCollapse.appendChild(paragraph) : buttonCollapse.removeChild(textOpen)
-
-            currentStep.classList.toggle('collapsed', !isCollapsed);
-            currentStep.setAttribute('aria-hidden', !isCollapsed);
-          },
+          action: () => modalCollapseHandler(tour),
           ...(step.btnCollapseClass && { classes: step.btnCollapseClass }),
         },
         {
           text: step.btnBackText,
           action() {
             if (index === 0) {
-              tour.cancel();
+              tour.complete();
             } else {
               if(step.urlMollie) {
                 window.open(`${step.urlMollie}/signin`, '_blank');
@@ -295,7 +304,7 @@ $(function () {
         {
           text: step.btnNextText,
           action() {
-            if(index === steps.length - 2) {
+            if(index === steps.length - 1) {
               tour.complete();
             } else {
               if(step.urlMollie) {
@@ -312,76 +321,107 @@ $(function () {
     });
   });
 
-  tour.start();
+ tour.start();
 
   const stepsOrderApi = [
     {
-      stepNoClass: 'step-1 right-bottom',
-      text: 'Choose Payments API (Payments API can not be used for methods such as Klarna - we\'ll set-up that up later)\n' +
-        'Learn about the difference between: Orders API or the Payments API ',
+      id: 'orderApi-1',
+      stepClass: 'right-bottom',
+      text: 'Select Orders API - this is Mollie\n' +
+        'suggested API to use for webshops b/c it allows you to create “orders”. An order contains the personal information of a customer (e.g. address) and products that the customer ordered. When an order is made, a corresponding payment will be created automatically.',
       btnBackText:'Go back',
       btnNextText: 'Next',
-      // attachToElement: '.onboardingWizard-paymentType .dropdown',
+      attachToElement: '.onboardingWizard-paymentType .dropdown',
+      btnNextClass: 'with-triangle',
+      btnCollapseClass: 'btn-collapse',
     },
   ];
 
-  const tourOrdersApi = new Shepherd.Tour({
-    useModalOverlay: true,
-    defaultStepOptions: {
-      arrow: false,
-      cancelIcon: {
-        enabled: true,
-      },
-      scrollTo: {
-        behavior: 'smooth',
+  function mountTourOrderApi () {
+    const select = document.querySelector(
+      '#sylius_payment_method_gatewayConfig_mollieGatewayConfig_1_paymentType ~ .menu'
+    )
+    const orderApiOption = select.querySelector('[data-value="ORDER_API"]');
+
+    if (!orderApiOption) {
+      return;
+    }
+
+    const orderApiOptionSelected =
+      $('[for=sylius_payment_method_gatewayConfig_mollieGatewayConfig_1_paymentType] + .dropdown')
+        .find(':selected')
+        .val() === 'ORDER_API'
+
+    select.addEventListener('click', (event) => {
+      if (!orderApiOptionSelected) {
+        event.target.click();
       }
-    },
-  });
-
-  stepsOrderApi.forEach((step, index) => {
-    tourOrdersApi.addStep({
-      title: step.title ? step.title : null,
-      text: step.text,
-      classes: step.stepNoClass,
-      attachTo: {
-        ...(step.attachToElement && { element: step.attachToElement }),
-      },
-      ...(step.classActive && { highlightClass: step.classActive }),
-      scrollToHandler: function () {
-        const target = document.querySelector(step.attachToElement);
-        console.log(target);
-        if (!target) {
-          return;
-        }
-
-        $('html,body').animate({
-          scrollTop: $(target).offset().top - 100
-        }, 500);
-
-        // window.scroll({
-        //   top: $(target).offset().top - 100,
-        // });
-
-        // window.scrollTo(0, target.getBoundingClientRect().top - 200);
-      },
-      buttons: [
-        {
-          text: step.btnBackText,
-          action: index === 0 ? tour.cancel : tour.back,
-          secondary: true,
-          ...(step.btnBackClass && { classes: step.btnBackClass }),
-        },
-        {
-          text: step.btnNextText,
-          action: index === steps.length - 1 ? tour.complete : tour.next,
-          ...(step.btnNextClass && { classes: step.btnNextClass }),
-        },
-      ],
     });
-  });
 
-  document.querySelector('[data-value="ORDER_API"]').addEventListener('click', () => {
-    tourOrdersApi.start();
-  })
+    orderApiOption.addEventListener('click', () => {
+      tour.addStep({
+        id: 'orderApi-1',
+        classActive: 'store-settings',
+        classes: 'right-bottom',
+        text: 'Select Orders API - this is Mollie\n' +
+          'suggested API to use for webshops b/c it allows you to create “orders”. An order contains the personal information of a customer (e.g. address) and products that the customer ordered. When an order is made, a corresponding payment will be created automatically.',
+        btnBackText:'Go back',
+        btnNextText: 'Next',
+        attachToElement: '.onboardingWizard-paymentType .dropdown',
+        when: {
+          show() {
+            const currentStep = this.tour.getCurrentStep().target;
+            this.previousStepIndex = tour.steps.indexOf(currentStep);
 
-});
+            if (!currentStep) {
+              return false;
+            }
+
+            navbarProgressHandler(currentStep);
+          },
+        },
+        buttons: [
+          {
+            text: '<i class="arrow down icon"></i>',
+            action() {
+              const currentStep = this.currentStep.el;
+              console.log(tour.steps.indexOf(currentStep));
+              const buttonCollapse = currentStep.querySelector('.btn-collapse');
+              const isCollapsed = [...currentStep.classList].includes('collapsed');
+
+              const paragraph = document.createElement('span');
+              paragraph.classList.add('btn-text-open');
+              paragraph.textContent = 'Open';
+
+              if (!buttonCollapse) {
+                return;
+              }
+
+              const textOpen = buttonCollapse.querySelector('.btn-text-open ')
+
+              !isCollapsed ? buttonCollapse.appendChild(paragraph) : buttonCollapse.removeChild(textOpen)
+
+              currentStep.classList.toggle('collapsed', !isCollapsed);
+              currentStep.setAttribute('aria-hidden', !isCollapsed);
+            },
+            classes: 'btn-collapse',
+          },
+          {
+            text: 'Go back',
+            action: () => tour.back(),
+            secondary: true,
+          },
+          {
+            text: 'Next',
+            action: () => {
+              tour.show('restrictPayment', true)
+            },
+          },
+        ],
+      });
+      tour.show('orderApi-1', true);
+    })
+  }
+
+  mountTourOrderApi();
+})
