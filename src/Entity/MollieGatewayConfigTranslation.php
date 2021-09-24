9a06966dd5bc8bb858a1093908e07c0e9fd4cab6
Marcin Bonk <marvcin@gmail.com>
2021-09-24 16:17:34 +0200

    Translatable payment labels

---
+++ src/Entity/MollieGatewayConfigTranslation.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace BitBag\SyliusMolliePlugin\Entity;
+
+use Sylius\Component\Resource\Model\AbstractTranslation;
+
+class MollieGatewayConfigTranslation extends AbstractTranslation implements MollieGatewayConfigTranslationInterface
+{
+    /** @var int|null */
+    protected $id;
+
+    /** @var string|null */
+    protected $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): void
+    {
+        $this->name = $name;
+    }
+}
---
+++ src/Entity/MollieGatewayConfigTranslationInterface.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace BitBag\SyliusMolliePlugin\Entity;
+
+use Sylius\Component\Resource\Model\ResourceInterface;
+use Sylius\Component\Resource\Model\TranslationInterface;
+
+interface MollieGatewayConfigTranslationInterface extends ResourceInterface, TranslationInterface
+{
+    public function getId(): ?int;
+
+    public function getName(): ?string;
+
+    public function setName(?string $name): void;
+}
