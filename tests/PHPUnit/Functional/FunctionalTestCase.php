a090b99e55a877387a61bc25a8de65b0f7d30ea2
marekrzytki <nooneusing@outlook.com>
2022-05-12 14:51:25 +0200

    Add check for method in KernelTestCase, add wkhtmltopdf package in test workflow

--- tests/PHPUnit/Functional/FunctionalTestCase.php
+++ tests/PHPUnit/Functional/FunctionalTestCase.php
@@ -5,13 +5,36 @@ declare(strict_types=1);
 namespace Tests\BitBag\SyliusMolliePlugin\PHPUnit\Functional;
 
 use ApiTestCase\JsonApiTestCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Webmozart\Assert\Assert;
 
 class FunctionalTestCase extends JsonApiTestCase
 {
-    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    public function __construct(
+        ?string $name = null,
+        array $data = [],
+        string $dataName = ''
+    )
     {
         parent::__construct($name, $data, $dataName);
 
         $this->dataFixturesPath = __DIR__ . '/DataFixtures';
+    }
+
+    protected function getContainerByVersion(): ContainerInterface
+    {
+        /**
+         * Function getContainer is not available before Symfony 5.3
+         */
+        if (method_exists(KernelTestCase::class, 'getContainer')) {
+            $container = self::getContainer();
+        } else {
+            $container = self::$container;
+        }
+
+        Assert::isInstanceOf($container, ContainerInterface::class);
+
+        return $container;
     }
 }
